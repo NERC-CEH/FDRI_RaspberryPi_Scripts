c677e5b19f9ec66ca82b091b926b866e71e1a467
Jo Walsh <jowals@ceh.ac.uk>
2025-06-13 15:32:07 +0100

    avoid importing the camera module in unit tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,5 @@
 from unittest.mock import MagicMock
 
-from raspberrycam.camera import DebugCamera
 from raspberrycam.core import Raspberrycam
 from raspberrycam.location import Location
 from raspberrycam.scheduler import FdriScheduler
@@ -10,10 +9,14 @@ class MockImageManager(MagicMock):
     pass
 
 
+class MockCamera(MagicMock):
+    pass
+
+
 def test_raspberrycam() -> None:
     """Will it run? -style test for the core interface"""
     location = Location(55.8626453, -3.2031049)
     sched = FdriScheduler(location)
-    cam = Raspberrycam(sched, DebugCamera(256, 256), MockImageManager())
+    cam = Raspberrycam(sched, MockCamera(256, 256), MockImageManager())
 
     assert isinstance(cam, Raspberrycam)
